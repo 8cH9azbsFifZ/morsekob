1aaaee4b140d4feabd48cd6aaa12c4d6162266c3
Gerolf Ziegenhain <g@ziegenhain.com>
2015-01-04 00:38:43 +0100

    added more applications

---
+++ Applications/MorseWF/config.py
@@ -0,0 +1,17 @@
+# MorseWF configuration settings
+
+# A line starting with a pound sign (#) is treated as a comment and is ignored by the system.
+
+#PORT = None
+PORT = 'COM7'
+
+#AUDIO = True
+AUDIO = False
+
+#JACKPORT = None
+JACKPORT = 'COM8'
+
+FONTNAME = ''  # default system font
+#FONTNAME = 'arial'
+#FONTNAME = 'timesnewroman'
+FONTSIZE = 28
